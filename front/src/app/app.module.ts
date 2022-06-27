a5747c48f96b6635f36d14dd89e6d4754926026d
sebastien <sebastien2.descamps@gmail.com>
2022-06-27 16:06:02 +0200

    feat: Ajout de la stucture basique de la navbar :sparkles:

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './_commons/navbar/navbar.component';
+import { FooterComponent } from './_commons/footer/footer.component';
+import { NotFoundComponent } from './_commons/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule
