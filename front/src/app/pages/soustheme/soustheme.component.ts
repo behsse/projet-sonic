50c24959153c9130e39b3dc08fb80c06c540163a
sebastien <sebastien2.descamps@gmail.com>
2022-06-28 13:57:24 +0200

    feat: Ajout des composants formation, home, soustheme, theme :sparkles:

---
+++ front/src/app/pages/soustheme/soustheme.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-soustheme',
+  templateUrl: './soustheme.component.html',
+  styleUrls: ['./soustheme.component.scss']
+})
+export class SousthemeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
