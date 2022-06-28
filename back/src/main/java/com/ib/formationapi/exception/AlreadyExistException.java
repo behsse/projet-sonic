5f83076b17dedc8324e8bcffc3c5183253fbe357
hanan <hananeanghari1@gmail.com>
2022-06-28 20:12:47 +0200

    feat: ajout des exception :label:

---
+++ back/src/main/java/com/ib/formationapi/exception/AlreadyExistException.java
@@ -0,0 +1,7 @@
+package com.ib.formationapi.exception;
+
+public class AlreadyExistException extends Exception{
+    public AlreadyExistException(String message) {
+        super(message);
+    }
+}
