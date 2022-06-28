8550efb4771b5b6efc94a0a9fb1bf178fccc38b7
sebastien <sebastien2.descamps@gmail.com>
2022-06-28 13:11:17 +0200

    feat: Ajout de la page 404 Not Found :sparkles:

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {LottieAnimationViewModule } from 'ng-lottie';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './_commons/navbar/navbar.component';
@@ -16,7 +17,8 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LottieAnimationViewModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
