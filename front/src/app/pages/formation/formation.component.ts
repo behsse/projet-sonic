680c4ee79e8930762d2b0832462946e845213ab6
sebastien <sebastien2.descamps@gmail.com>
2022-06-29 11:47:58 +0200

    feat: Ajout du header de la page formation :sparkles:

--- front/src/app/pages/formation/formation.component.ts
+++ front/src/app/pages/formation/formation.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+// Import des models
+import Formation from 'src/app/models/formation.model';
+
+// Import des services
+import { FormationService } from 'src/app/service/formation.service';
 
 @Component({
   selector: 'app-formation',
@@ -7,9 +14,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormationComponent implements OnInit {
 
-  constructor() { }
+  formation!: Formation;
+
+  constructor(private router: Router, private route: ActivatedRoute, private formationService: FormationService) { }
 
   ngOnInit(): void {
+    const type = this.route.snapshot.paramMap.get('type');
+    const id = this.route.snapshot.paramMap.get('id');
+    // this.setSubscribe(type, id);
   }
+
+  // private subscribeFormation(id: number): void{
+  //   this.formationService.getFormation(id).subscribe((formation : Formation) => {
+  //     this.formation = formation;
+  //   })
+  // }
+
+  // private setSubscribe(type: string | null, id: string | null){
+  //   if (id && type == 'formations'){
+  //     this.subscribeFormation(+id);
+  //   }else if (!id || !type){
+  //     this.router.navigate(['/not-found']);
+  //   }
+  // }
 
 }
