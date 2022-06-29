a53bd68a7849ebeb771bbdcd20ecf36e006f3213
sebastien <sebastien2.descamps@gmail.com>
2022-06-29 11:46:57 +0200

    feat: Ajout de la route formation :sparkles:

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormationComponent } from './pages/formation/formation.component';
 import { NotFoundComponent } from './_commons/not-found/not-found.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'formations', component: FormationComponent},
   {path: '**', component: NotFoundComponent},
 ];
 
