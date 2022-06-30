af677705f893a6913e40c2039362e7bd89468fb0
hanan <hananeanghari1@gmail.com>
2022-06-30 11:33:02 +0200

    feat: transformer un boolean qu'on a en java en TINYINT(1) en base de donnée :sparkles:

--- back/src/main/java/com/ib/formationapi/entity/Theme.java
+++ back/src/main/java/com/ib/formationapi/entity/Theme.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.persistence.*;
 import java.util.List;
@@ -14,18 +15,18 @@ import java.util.List;
 @AllArgsConstructor
 public class Theme {
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String titre;
-    @OneToMany(mappedBy = "themeParent", cascade  = CascadeType.ALL)
+    @OneToMany(mappedBy = "themeParent", cascade = CascadeType.ALL)
     private List<Theme> sousThemeListe;
     @ManyToOne
     @JoinColumn(referencedColumnName = "id")
     @JsonBackReference
     private Theme themeParent;
-    @Column(name = "est_sous_theme")
+    @Column(name = "est_sous_theme", nullable = false, columnDefinition = "TINYINT(1)")
     private boolean estSousTheme;
-    @OneToMany(mappedBy = "theme", cascade  = CascadeType.ALL)
+    @OneToMany(mappedBy = "theme", cascade = CascadeType.ALL)
     private List<Formation> formationsList;
     @ManyToMany(mappedBy = "themesListe")
     private List<Domaine> domaineListe;
