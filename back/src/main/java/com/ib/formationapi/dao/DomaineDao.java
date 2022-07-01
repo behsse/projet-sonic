dac70af02210ac8714b9bb3ab7ba50bdd1e06c36
hanan <hananeanghari1@gmail.com>
2022-07-01 12:19:45 +0200

    fix: Modification de la  documentation java :sparkles:

--- back/src/main/java/com/ib/formationapi/dao/DomaineDao.java
+++ back/src/main/java/com/ib/formationapi/dao/DomaineDao.java
@@ -1,7 +1,6 @@
 package com.ib.formationapi.dao;
 
 import com.ib.formationapi.entity.Domaine;
-import com.ib.formationapi.entity.Formation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,10 +9,10 @@ import java.util.Optional;
 @Repository
 public interface DomaineDao extends JpaRepository<Domaine, Long> {
     /**
-     * c'une méthode qui permet de récuperer un domaine à partir de son nom
+     * c'est une méthode qui permet de récuperer un domaine à partir de son nom
      *
      * @param intitule l' intitule du domaine qu'on cherche
-     * @return une formation
+     * @return un domaine
      */
     public Optional<Domaine> findByIntitule(String intitule);
 }
