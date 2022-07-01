35f3578366b59bf4c7e3c6288b2610ffd9680959
sebastien <sebastien2.descamps@gmail.com>
2022-07-01 11:19:04 +0200

    feat: Ajout des domaines dans la navbar :sparkles:

--- front/src/app/_commons/navbar/navbar.component.ts
+++ front/src/app/_commons/navbar/navbar.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import Domaine from 'src/app/models/domaine.model';
+
+import Formation from 'src/app/models/formation.model';
+import { DomaineService } from 'src/app/service/domaine.service';
+import { FormationService } from 'src/app/service/formation.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +12,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  formations: Formation[] = [];
+  domaines : Domaine[] = [];
+
+  constructor(private formationService: FormationService, private domaineService : DomaineService) { }
 
   ngOnInit(): void {
+
+    this.formationService.getFormations().subscribe((formation) => {this.formations = formation})
+    this.domaineService.getDomaines().subscribe((domaine) => {this.domaines = domaine});
+
   }
 
 }
