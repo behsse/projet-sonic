8ec0c3d483f76092f4a73c595f2035c0a1dd2e46
sebastien <sebastien2.descamps@gmail.com>
2022-07-01 11:16:47 +0200

    feat: Ajout des méthodes CRUD :sparkles:

--- front/src/app/service/session.service.ts
+++ front/src/app/service/session.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import Session from '../models/session.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionService {
 
-  constructor() { }
+  private apiUrl = "http://localhost:8080"
+
+  constructor(private httpClient : HttpClient) { }
+
+  getSessions(): Observable<Session[]>{
+    return this.httpClient.get<Session[]>(`${this.apiUrl}/sessions`);
+  }
+
+  getSession(id: number): Observable<Session>{
+    return this.httpClient.get<Session>(`${this.apiUrl}/sessions/id/${id}`);
+  }
+
+  createSession(Session: Session): Observable<Session>{
+    return this.httpClient.post<Session>(`${this.apiUrl}/sessions`, Session);
+  }
+
+  updateSession(Session: Session): Observable<Session>{
+    return this.httpClient.put<Session>(`${this.apiUrl}/sessions/${Session.id}`, Session);
+  }
+
+  deleteSession(id: number): Observable<Session>{
+    return this.httpClient.delete<Session>(`${this.apiUrl}/sessions/${id}`);
+  }
 }
